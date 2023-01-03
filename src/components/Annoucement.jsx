0c1ddb8724465e962c4eff244579987219457a33
TrongHien18 <67732499+chopper99it@users.noreply.github.com>
2023-01-03 10:49:37 +0700

    feat. Create Annoucement

--- src/components/Annoucement.jsx
+++ src/components/Annoucement.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Annoucement = () => {
-  return <div>Annoucement</div>;
+  return (
+    <>
+      <div className="">
+        <div className="bg-blue-700 text-white text-center mx-auto px-4 py-3">
+          <p>
+            Love Alpine JS? Check out this new course!{" "}
+            <a className="underline" href="">
+              Learn More
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Annoucement;
